29235a7414f171da8bde7c4657861b7436db9d2d
Anurag <anuragsingh.rawat@infibeam.net>
2014-02-21 04:07:30 +0530

    setting current user in model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :set_current_user
+
+  def set_current_user
+    User.current = current_user
+  end
+
+  def after_sign_in_path_for(resource)
+    request.env['omniauth.origin'] || stored_location_for(resource) || root_path
+  end
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, :alert => exception.message
